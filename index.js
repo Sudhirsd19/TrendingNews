b7879c81e454aa346111d5ffd8e4c94c9244173f
Sudhirsd19 <skdsd19999@gmail.com>
2026-04-26 21:41:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Step 1 working successfully");
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+async function run() {
+
+  await db.collection("test")
+    .doc("demo")
+    .set({
+      message: "Firebase connected successfully",
+      time: new Date()
+    });
+
+  console.log("Firebase connected ✅");
+}
+
+run();
